33d63a4001f381d0b6fbe5bda996e85c3979bfdb
Mohammed <moody54324@gmail.com>
2022-03-03 03:16:21 +0300

    Rename HandleSys && /*.java => DealingSys && /Deal**.java

---
+++ src/main/java/Core/DealingSys/Dealer.java
@@ -0,0 +1,8 @@
+package Core.DealingSys;
+
+
+// This is the 'current dealer'. this should be the delegator of the player.isDealer/LeftToDealer;
+public class Dealer {
+
+    // Should be assigned here;
+}
